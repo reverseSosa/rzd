1c738695ef46c8e6ea43b08229671c00b99dfe72
unknown <skrfiness@gmail.com>
2023-11-22 22:48:48 +0300

    fix

--- app/[locale]/[stationId]/(root)/page.tsx
+++ app/[locale]/[stationId]/(root)/page.tsx
@@ -9,39 +9,69 @@ export default async function Home({
 	params,
 }: { params: { locale: "ru" | "en"; stationId: StationId } }) {
 	const t = await getScopedI18n(`${params.stationId}.home`);
-	const w = 1;
 
-	const data = await fetch(
-		`https://rzd.bpium.ru/api/webrequest/station?id=${params.stationId}`,
-		{ method: "GET" },
-	).then((res) => res.json());
+	try {
+		const data = await fetch(
+			`https://rzd.bpium.ru/api/webrequest/station?id=${params.stationId}`,
+			{ method: "GET" },
+		).then((res) => res.json());
 
-	return (
-		<main className="flex min-h-screen flex-col items-center pt-[360px] text-center">
-			<div className="absolute top-0 left-0 z-0 max-w-full overflow-hidden h-[800px]">
-				<video
-					id="video_background"
-					preload="auto"
-					autoPlay
-					loop
-					playsInline
-					muted
-					className="opacity-30 z-0"
-					style={{ transform: "scale(1.5)" }}
-					src="/nebo.webm"
-				/>
-				<div className="w-full h-[800px] bg-gradient-to-t from-white to-transparent z-[5] absolute top-0 left-0" />
-			</div>
+		return (
+			<main className="flex min-h-screen flex-col items-center pt-[360px] text-center">
+				<div className="absolute top-0 left-0 z-0 max-w-full overflow-hidden h-[800px]">
+					<video
+						id="video_background"
+						preload="auto"
+						autoPlay
+						loop
+						playsInline
+						muted
+						className="opacity-30 z-0"
+						style={{ transform: "scale(1.5)" }}
+						src="/nebo.webm"
+					/>
+					<div className="w-full h-[800px] bg-gradient-to-t from-white to-transparent z-[5] absolute top-0 left-0" />
+				</div>
 
-			<h2 className="text-[30px] z-10">{t("station")}</h2>
-			<h1 className="text-[50px] font-bold text-red z-10">
-				{params.locale === "ru" ? data.values["2"] : data.values["3"]}
-			</h1>
-			<p className="text-[15px] leading-[25px] mt-[39px] max-w-[800px] z-10">
-				{params.locale === "ru" ? data.values["5"] : data.values["6"]}
-			</p>
-			<NavBlock stationId={params.stationId} />
-			<RoutesBlock stationId={params.stationId} />
-		</main>
-	);
+				<h2 className="text-[30px] z-10">{t("station")}</h2>
+				<h1 className="text-[50px] font-bold text-red z-10">
+					{params.locale === "ru" ? data.values["2"] : data.values["3"]}
+				</h1>
+				<p className="text-[15px] leading-[25px] mt-[39px] max-w-[800px] z-10">
+					{params.locale === "ru" ? data.values["5"] : data.values["6"]}
+				</p>
+				<NavBlock stationId={params.stationId} />
+				<RoutesBlock stationId={params.stationId} />
+			</main>
+		);
+	} catch (error) {
+		console.log(error);
+
+		return (
+			<main className="flex min-h-screen flex-col items-center pt-[360px] text-center">
+				<div className="absolute top-0 left-0 z-0 max-w-full overflow-hidden h-[800px]">
+					<video
+						id="video_background"
+						preload="auto"
+						autoPlay
+						loop
+						playsInline
+						muted
+						className="opacity-30 z-0"
+						style={{ transform: "scale(1.5)" }}
+						src="/nebo.webm"
+					/>
+					<div className="w-full h-[800px] bg-gradient-to-t from-white to-transparent z-[5] absolute top-0 left-0" />
+				</div>
+
+				<h2 className="text-[30px] z-10">{t("station")}</h2>
+				<h1 className="text-[50px] font-bold text-red z-10">Какой-то вокзал</h1>
+				<p className="text-[15px] leading-[25px] mt-[39px] max-w-[800px] z-10">
+					Очень крутой
+				</p>
+				<NavBlock stationId={params.stationId} />
+				<RoutesBlock stationId={params.stationId} />
+			</main>
+		);
+	}
 }
--- app/[locale]/[stationId]/(routes)/about/page.tsx
+++ app/[locale]/[stationId]/(routes)/about/page.tsx
@@ -14,62 +14,112 @@ const AboutPage = async ({
 }: { params: { locale: "ru" | "en"; stationId: StationId } }) => {
 	const t = await getScopedI18n(`${params.stationId}.aboutStation`);
 
-	const data = await fetch(
-		`https://rzd.bpium.ru/api/webrequest/station?id=${params.stationId}`,
-		{ method: "GET" },
-	).then((res) => res.json());
+	try {
+		const data = await fetch(
+			`https://rzd.bpium.ru/api/webrequest/station?id=${params.stationId}`,
+			{ method: "GET" },
+		).then((res) => res.json());
 
-	const images = data.values["12"].map((image: any) => image.url);
+		const images = data.values["12"].map((image: any) => image.url);
 
-	return (
-		<main className="flex min-h-screen flex-col items-center">
-			<MapClient stationId={params.stationId} />
-			<div className="bg-red w-full px-[100px] py-20 flex items-center gap-[60px] text-white">
-				<div className="h-full py-14 px-[50px] border border-white">
-					<TrainIcon className="h-[60px] w-[60px] stroke-white" />
+		return (
+			<main className="flex min-h-screen flex-col items-center">
+				<MapClient stationId={params.stationId} />
+				<div className="bg-red w-full px-[100px] py-20 flex items-center gap-[60px] text-white">
+					<div className="h-full py-14 px-[50px] border border-white">
+						<TrainIcon className="h-[60px] w-[60px] stroke-white" />
+					</div>
+					<div className="text-left h-full">
+						<h2 className="text-[20px]">{t("railwayStation")}</h2>
+						<h1 className="text-[40px] font-bold mt-2">
+							{params.locale === "ru" ? data.values["2"] : data.values["3"]}
+						</h1>
+						<span className="text-[20px] font-bold leading-[30px] mt-4">
+							+7 800 775-00-00
+						</span>
+						<p className="text-[15px] mt-2 font-normal">
+							{data.values["8"].value}
+						</p>
+					</div>
 				</div>
-				<div className="text-left h-full">
-					<h2 className="text-[20px]">{t("railwayStation")}</h2>
-					<h1 className="text-[40px] font-bold mt-2">
-						{params.locale === "ru" ? data.values["2"] : data.values["3"]}
-					</h1>
-					<span className="text-[20px] font-bold leading-[30px] mt-4">
-						+7 800 775-00-00
-					</span>
-					<p className="text-[15px] mt-2 font-normal">
-						{data.values["8"].value}
-					</p>
+				<div className="px-[100px] pt-[60px] pb-[91px]">
+					<div className="text-[15px] leading-[25px] text-left flex flex-col gap-8">
+						<p className="font-bold">{t("mainP")}</p>
+						<p>{t("secondP")}</p>
+						<p>{t("thirdP")}</p>
+						<p>{t("fourthP")}</p>
+						<p>{t("fifthP")}</p>
+						<p>{t("sixthP")}</p>
+						<p>{t("seventhP")}</p>
+					</div>
 				</div>
-			</div>
-			<div className="px-[100px] pt-[60px] pb-[91px]">
-				<div className="text-[15px] leading-[25px] text-left flex flex-col gap-8">
-					<p className="font-bold">{t("mainP")}</p>
-					<p>{t("secondP")}</p>
-					<p>{t("thirdP")}</p>
-					<p>{t("fourthP")}</p>
-					<p>{t("fifthP")}</p>
-					<p>{t("sixthP")}</p>
-					<p>{t("seventhP")}</p>
+				<Slider images={images} />
+				<div className="px-[100px] pt-20 pb-[66px]">
+					<div className="text-[15px] leading-[25px] text-left flex flex-col gap-8 mb-[54px]">
+						<p className="font-bold">{t("mainP")}</p>
+						<p>{t("secondP")}</p>
+						<p>{t("thirdP")}</p>
+						<p>{t("fourthP")}</p>
+						<p>{t("fifthP")}</p>
+						<p>{t("sixthP")}</p>
+						<p>{t("seventhP")}</p>
+					</div>
+					<div className="flex flex-col gap-5">
+						<ToTopButton />
+						<MainButton />
+					</div>
 				</div>
-			</div>
-			<Slider images={images} />
-			<div className="px-[100px] pt-20 pb-[66px]">
-				<div className="text-[15px] leading-[25px] text-left flex flex-col gap-8 mb-[54px]">
-					<p className="font-bold">{t("mainP")}</p>
-					<p>{t("secondP")}</p>
-					<p>{t("thirdP")}</p>
-					<p>{t("fourthP")}</p>
-					<p>{t("fifthP")}</p>
-					<p>{t("sixthP")}</p>
-					<p>{t("seventhP")}</p>
+			</main>
+		);
+	} catch (error) {
+		console.log(error);
+
+		return (
+			<main className="flex min-h-screen flex-col items-center">
+				<MapClient stationId={params.stationId} />
+				<div className="bg-red w-full px-[100px] py-20 flex items-center gap-[60px] text-white">
+					<div className="h-full py-14 px-[50px] border border-white">
+						<TrainIcon className="h-[60px] w-[60px] stroke-white" />
+					</div>
+					<div className="text-left h-full">
+						<h2 className="text-[20px]">{t("railwayStation")}</h2>
+						<h1 className="text-[40px] font-bold mt-2">Какой-то вокзал</h1>
+						<span className="text-[20px] font-bold leading-[30px] mt-4">
+							+7 800 775-00-00
+						</span>
+						<p className="text-[15px] mt-2 font-normal">Очень крутой</p>
+					</div>
 				</div>
-				<div className="flex flex-col gap-5">
-					<ToTopButton />
-					<MainButton />
+				<div className="px-[100px] pt-[60px] pb-[91px]">
+					<div className="text-[15px] leading-[25px] text-left flex flex-col gap-8">
+						<p className="font-bold">{t("mainP")}</p>
+						<p>{t("secondP")}</p>
+						<p>{t("thirdP")}</p>
+						<p>{t("fourthP")}</p>
+						<p>{t("fifthP")}</p>
+						<p>{t("sixthP")}</p>
+						<p>{t("seventhP")}</p>
+					</div>
 				</div>
-			</div>
-		</main>
-	);
+				<Slider images={[]} />
+				<div className="px-[100px] pt-20 pb-[66px]">
+					<div className="text-[15px] leading-[25px] text-left flex flex-col gap-8 mb-[54px]">
+						<p className="font-bold">{t("mainP")}</p>
+						<p>{t("secondP")}</p>
+						<p>{t("thirdP")}</p>
+						<p>{t("fourthP")}</p>
+						<p>{t("fifthP")}</p>
+						<p>{t("sixthP")}</p>
+						<p>{t("seventhP")}</p>
+					</div>
+					<div className="flex flex-col gap-5">
+						<ToTopButton />
+						<MainButton />
+					</div>
+				</div>
+			</main>
+		);
+	}
 };
 
 export default AboutPage;
